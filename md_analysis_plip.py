586e73dd326cd4d9bf668597cb75cc24bf5434e4
Melchor Sanchez Martinez <@msanchezmartinez4>
2021-05-21 00:00:38 +0200

    05/21 post

---
+++ md_analysis_plip.py
@@ -0,0 +1,34 @@
+import pytraj as pt
+import seaborn as sns
+import pylab as plt
+import numpy as np
+sns.set()
+
+def run_command(command):
+    createPath('./logs')
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        print (output)
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print (output.strip())
+    rc = process.poll()
+    return rc
+
+#Load the first 100 frames of the trajectory from 10 to 10
+traj=pt.iterload('/home/melchor/science/papers/paper_laura/test/target_ligand_\
+solvent-w-MD.dcd', top='/home/melchor/science/papers/paper_laura/test/\
+target_ligand_solvent.prmtop', frame_slice=[(0, 100, 10),])
+traj=traj.strip(':WAT,:Na+')
+
+
+count=0
+for frame in traj:
+    count=count+1
+    pt.write_traj('trajectory_frame'+str(count)+'.pdb', traj=traj, overwrite=True)
+    pt.write_traj('trajectory.pdb', traj=traj, overwrite=True)
+
+plip=('')
+run_command(plip)
